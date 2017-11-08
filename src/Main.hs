43e0ee5a089d4fb9d5812e0f66413df1a32c8c9f
rpeszek <peszeks@gmail.com>
2017-11-08 16:22:38 -0700

    Eta Issue 552

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.ByteString.Lazy.Char8 as LBS8
+import qualified Network.Mime as M
 
 main :: IO ()
-main = 
-    do
-        myLBS <- LBS.readFile "src/Main.hs"
-        putStrLn $ "Read File"
-
-        {- these lines will exception -}
-        let lbsLength = LBS.length myLBS
-        putStrLn $ "Length " ++ (show lbsLength)
-
-        {- If instead of the above lines I try the following then
-           the program prints this text as expected but exceptions follow -}
-        -- LBS8.putStrLn myLBS
+main = do print $ M.mimeByExt M.defaultMimeMap "application/text" "test.js"
+  
